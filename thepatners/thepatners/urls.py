adf34aa5f43daa5f28d197968fec10928b04b270
achmad <achmadqusayiri291017@gmail.com>
2022-09-28 15:06:36 +0700

    admin lte

--- thepatners/thepatners/urls.py
+++ thepatners/thepatners/urls.py
@@ -16,12 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.http import HttpResponse
-from .views import home , about
-
+from .views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', home),
-    path('about/', about),
     
 ]
